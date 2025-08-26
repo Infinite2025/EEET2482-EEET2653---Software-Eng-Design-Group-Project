d13c0b5a9ac6d3824c46f4846cea499efdffbb68
Qanh56 <s4118973@rmit.edu.vn>
2025-08-26 14:26:01 +0700

    Update EBike.h

--- src/EBike.h
+++ src/EBike.h
@@ -10,6 +10,9 @@ class Ebike{
         Ebike(); // create a default Constructor
 
         Ebike(const string& brand, const string& model, const string& color, int engineSize_cc, int year, const string& plateNum); //Create a Constructor with all the information of 1 bike
+
+        bool Listing(const string& start, const string& end, int dailyRate, int minRate, bool available) //Create information for listing with start and end day, CP daily, minimum rating of user, the bike is available or not
+
         
 
 }
