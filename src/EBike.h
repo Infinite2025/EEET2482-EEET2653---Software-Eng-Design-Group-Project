06822d6e781ee01dee9dab2d380ac29851c78501
Infinite2025 <trinhtuankiet8306@gmail.com>
2025-09-03 09:37:18 +0700

    Minor fixes

--- src/EBike.h
+++ src/EBike.h
@@ -11,8 +11,10 @@ class Ebike{
 
         Ebike(const string& brand, const string& model, const string& color, int engineSize_cc, int year, const string& plateNum); //Create a Constructor with all the information of 1 bike
 
-        bool Listing(const string& start, const string& end, int dailyRate, int minRate, bool available) //Create information for listing with start and end day, CP daily, minimum rating of user, the bike is available or not
+        bool Listing(const string& start, const string& end, int dailyRate, int minRate, bool available); //Create information for listing with start and end day, CP daily, minimum rating of user, the bike is available or not
 
         
 
-}
+};
+
+#endif // EBIKE_H
